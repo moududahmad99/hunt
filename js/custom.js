b0fb3939c05a00ed1b8afb8ad69493d304d85aad
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-01-02 23:07:48 +0600

    Service Area Complete

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$('.service-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    centerMode: true,
+    centerPadding: '0',
+    speed: 1500,
+    prevArrow: '<i class="fa-solid fa-arrow-up arrow-up"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-down arrow-down"></i>',
+
+});
